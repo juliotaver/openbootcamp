961ed01d17561068146df2e013de33ff44cf5e97
Julio Tavera <julztavera@gmail.com>
2022-04-28 16:42:18 -0500

    primer git

---
+++ ejercicio2.js
@@ -0,0 +1,10 @@
+
+let cadena = "Soy una cadena de texto";
+
+function cadenar() {
+  let cadena = "Soy una cadena adentro de cadenar!";
+  console.log(cadena);
+}
+
+cadenar();
+console.log(cadena);
---
+++ mi-primer-proyecto-de-node/index.js
@@ -0,0 +1,11 @@
+
+// este es el mensaje de bienvenida
+console.log("esta es la puerta de entrada al proyecto")
+
+/* esta es la parte intermedia de nuestro proyecto
+en esta parte realizamos los chequeos necesarios
+por ultimo vamos a la ultima linea de saludo */
+
+
+// Este va a ser el mensaje que se envia para despedir
+console.log("Adios")
