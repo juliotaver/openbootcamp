1258a33a108a900282082dba26db92617dbb2d1a
Julio Tavera <julztavera@gmail.com>
2022-04-28 22:49:59 -0500

    ejercicio3

---
+++ ejercicio3.js
@@ -0,0 +1,9 @@
+for (let i = 0; i < 101; i++) {
+    //Esto es el bucle
+    console.log(i);
+    if (i % 2 === 0) {
+        console.log("numero par")
+    } else {
+        console.log("numero impar")
+    }
+}
