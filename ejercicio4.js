ed6b02d05bf5e06c22f14908016f7c0d3efbe2ce
Julio Tavera <julztavera@gmail.com>
2022-05-03 23:05:15 -0500

    ejercicios

---
+++ ejercicio4.js
@@ -0,0 +1,2 @@
+let frase = "El perro de mi vecina es muy molesto" 
+console.log(frase.replace("vecina", "vecino"))
---
+++ ejercicio5.js
@@ -0,0 +1,2 @@
+let frase = "Este texto se convertira magicamente en mayusculas, con ayuda de nuestro amigo javascript"
+console.log(frase.toUpperCase())
