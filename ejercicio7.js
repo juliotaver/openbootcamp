06f8eeb81c57ee2946ac7838208d5b269f859aae
Julio Tavera <julztavera@gmail.com>
2022-05-19 13:19:41 -0500

    ejercicio 7

---
+++ ejercicio7.js
@@ -0,0 +1,7 @@
+let altura = 178;
+let altura_metros = 1.78;
+let peso = 90.8;
+let altura_metRedon = Math.round(altura_metros);
+let pesoRedon = Math.floor(peso);
+
+let maximovalor= "el maximo valor que se puede obtener en Javascripto + 1" + Number.MAX_VALUE;
